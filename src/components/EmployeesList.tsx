5520f9c530941e28340aecac985fe989d6a649cb
dekel <dekel009@gmail.com>
2024-04-24 20:09:13 +0300

    add styles for employees list

--- src/components/EmployeesList.tsx
+++ src/components/EmployeesList.tsx
@@ -1,3 +1,14 @@
+import {
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from "@chakra-ui/react";
+
 export interface Employee {
   name: string;
   _id: string;
@@ -12,13 +23,31 @@ interface Props {
 const EmployeesList = ({ employees }: Props) => {
   const list = employees || [];
   return (
-    <ul>
-      {list.map(({ _id, name, position, salary }) => (
-        <li
-          key={_id}
-        >{`id: ${_id}, name: ${name}, position: ${position}, salary: ${salary}`}</li>
-      ))}
-    </ul>
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption color={"gray"}>
+          List of employees that return as response from the server
+        </TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Employee ID</Th>
+            <Th>Name</Th>
+            <Th>Position</Th>
+            <Th isNumeric>Salary</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {list.map(({ _id, name, position, salary }) => (
+            <Tr key={_id}>
+              <Td>{_id}</Td>
+              <Td>{name}</Td>
+              <Td>{position}</Td>
+              <Td isNumeric>{salary}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 };
 
